58ea1a21e92384c067669cc64aca40a3cc76d9a9
Kagweitis <kagwegeorge82@gmail.com>
2025-05-16 16:58:16 +0300

    [Minor fix] Fixed the gateway unable to redirect to service issue

--- CustomerService/src/main/java/com/dtb/customerservice/Controller/CustomerController.java
+++ CustomerService/src/main/java/com/dtb/customerservice/Controller/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("customers/api/v1")
+@RequestMapping("/customers/api/v1")
 @RequiredArgsConstructor
 public class CustomerController {
 
